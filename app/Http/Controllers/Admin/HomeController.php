c94445391370e150233a582f9966214d97e8ec5a
Vito Farenga <vitofarenga@libero.it>
2022-09-19 16:30:30 +0200

    admin controller e modifica della route

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,10 +12,10 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
+     /* public function __construct()
     {
         $this->middleware('auth');
-    }
+    } */
 
     /**
      * Show the application dashboard.
@@ -26,3 +27,4 @@ class HomeController extends Controller
         return view('home');
     }
 }
+
